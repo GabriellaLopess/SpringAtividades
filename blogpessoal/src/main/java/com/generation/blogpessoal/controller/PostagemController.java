db6af1c32800318728d28a60f5c2216b3507301e
Gabriella Lopes <gabriellalopes0630@gmail.com>
2021-06-24 20:56:37 -0300

    First Commit

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,20 @@
+package com.generation.blogpessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.generation.blogpessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	
+	@Autowired
+	private PostagemRepository repository;
+
+}
