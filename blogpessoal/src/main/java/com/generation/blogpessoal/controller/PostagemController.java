965bc54eb96f66c5b51e2fa8927d7d9ea6612c2b
Gabriella <85173132+GabriellaLopess@users.noreply.github.com>
2021-06-24 19:42:07 -0300

    Delete blogpessoal directory

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++
@@ -1,5 +0,0 @@
-package com.generation.blogpessoal.controller;
-
-public class PostagemController {
-
-}
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++
@@ -1,5 +0,0 @@
-package com.generation.blogpessoal.repository;
-
-public interface PostagemRepository {
-
-}
